cd05114a6f7bfb33cc708c821ffb7bc2014b6a75
Cynart <123117216+Cynart@users.noreply.github.com>
2023-03-13 14:20:23 -0300

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,24 @@
+<style>
+footer {
+    background-color: #2e498e;
+    padding: 5px;
+    color:#ffffff;
+    text-align: center;
+    cursor:pointer;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+    z-index: 4;
+}
+footer a:visited, 
+footer a:link {
+    color: #ffffff;
+    text-decoration: none;
+}
+</style>
+
+</body>
+<footer>
+        <a href="index.php">Halifax Canoe and Kayak home</a>
+ </footer> 
+</html>
