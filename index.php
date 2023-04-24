60ca0627b1aec7cec83be15ab6ac49d1bfacd5e6
Cynart <123117216+Cynart@users.noreply.github.com>
2023-04-24 11:26:20 -0300

    last minute fix

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <br /> <br />
     
     <form action="problem.php" method="POST">
-    Full name: <input type="text" name="fullname">
+    <label>Full name </label><input type="text" name="fullname">
 
     <select name="role">
             <option value = "Admin">Admin</option>
