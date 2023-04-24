d2611e2f670ea08a53583a470a0689f8f487bb53
Cynart <123117216+Cynart@users.noreply.github.com>
2023-04-23 21:13:47 -0300

    Updated with whole project

--- index.php
+++ index.php
@@ -1,39 +1,25 @@
 <?php  session_start(); ?>
 <?php include 'header.php';  ?>
-
+<?php $fullname = $role = ""; ?>
 
     <main>
     <div class="form1">
-    <div class="formtitle">Welcome to Your IT Support System</div>
+    <div class="formtitle">Welcome to Your IT Support System
     <br /> <br />
     
-    <form action="welcome.php" method="POST">
-    <div class="formtext"> 
-        <select id="title" name="title">
-            <option value="Ms">Ms.</option>
-            <option value="Mr">Mr.</option>
-            <option value="Mrs">Mrs.</option>
-        </select>
-        <label for="fName" class="fName">First Name</label>
-                <input type="fName" name="fName" id="fName">
+    <form action="problem.php" method="POST">
+    <input type="text" name="fullname">
 
-        <label for="fName" class="lName">Last Name</label>
-                <input type="lName" name="lName" id="lName">
-
-        <select id="role" class="role">
+    <select name="role">
             <option value = "Admin">Admin</option>
-            <option value="Manager">Manager</option>
-            <option value="CEO">CEO</option>
+            <option value = "Manager">Manager</option>
+            <option value = "CEO">CEO</option>
         </select>
-
-        <input type="submit">
-    </div>
-
-
+     <button type="submit" name="login">Login</button>
 </form>
     
-    
     </div>
+        </div>
        
     </main>
    
