8f312e4729011d13bf0d87b2374051f918c5148a
Cynart <123117216+Cynart@users.noreply.github.com>
2023-04-23 21:11:35 -0300

    Add files via upload

---
+++ email_password.php
@@ -0,0 +1,17 @@
+
+<?php include 'header.php';  ?>
+<?php $email = ""; ?>
+
+<div class="formresponse">
+
+Email with password sent to 
+<?php echo $_POST["email"]; ?>
+
+<br /><br /><br /><br />
+<a href='logout.php'>Log out</a>
+
+</div>
+    
+
+
+<?php include 'footer.php';  ?>
